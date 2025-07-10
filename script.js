acf45dd0785b4b2b37027aa3733f808c6b040b7d
Saranya Barani <62207142+Saranya-git-20@users.noreply.github.com>
2025-07-10 19:18:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,30 @@
 // Complete the js code
 function Car(make, model) {
-	console.log("jiiiii")
+	this.make = make;
+	this.model = model;
 }
 
-function SportsCar(make, model, topSpeed) {}
+Car.prototype.getMakeModel = function () {
+  return this.make + " " + this.model;
+};
 
+function SportsCar(make, model, topSpeed) {
+	car.call(this,make,model)
+	this.topSpeed = topSpeed;
+}
+SportsCar.prototype = Object.create(Car.prototype);
+
+SportsCar.prototype.constructor = SportsCar;
+
+SportsCar.prototype.getTopSpeed = function () {
+	return this.topSpeed;
+}
+const myCar = new Car("Toyota", "Corolla");
+console.log(myCar.getMakeModel()); // Toyota Corolla
+
+const mySportsCar = new SportsCar("Ferrari", "488", 211);
+console.log(mySportsCar.getMakeModel()); // Ferrari 488
+console.log(mySportsCar.getTopSpeed());  // 211
 // Do not change the code below
 window.Car = Car;
 window.SportsCar = SportsCar;
